76e21556455f90f2dc32529e662c9d3088b1cbbc
Milan938 <23mboga@athenian.org>
2020-09-15 00:18:25 +0000

    Finished

--- my_code.py
+++ my_code.py
@@ -2,16 +2,23 @@
 #  
 
 def find_gcf(x,y):   # Do not change function name!
+    factor = x
+    while factor > 0:
+        if (x%factor) == 0:
+            if (y%factor) == 0:
+                return factor
+
+        factor = factor - 1
+    
 
     
 
-x = 21
-y = 14
+
 
 if __name__ == '__main__':
     # Test your code with this first
     # Change the argument to try different values
-    print(find_gcf(6,9))
+    print(find_gcf(21, 14))
 
     # After you are satisfied with your results, use input() to prompt the user for two values:
     #x = int(input("Enter a number: "))
